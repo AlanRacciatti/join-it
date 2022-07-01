4d52ba4c3bfa3b15f605ce3146ab816b42a930ed
AlanRacciatti <alanracciatti1220@gmail.com>
2022-07-01 20:43:24 -0300

    test: add whitelist tests

---
+++ smartcontract/test/whitelist.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Whitelist", function () {
+  it("Should be able to whitelist users", async () => {});
+  it("Should emit an event after whitelisting an user", async () => {});
+  it("Should revert if the user is already whitelisted", async () => {});
+  it("Should revert if limit of addresses was reached", async () => {});
+});
