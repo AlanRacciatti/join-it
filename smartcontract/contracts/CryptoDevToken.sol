0d9650b7464f266e04b3fd43e419ff077b8a8ba0
AlanRacciatti <alanracciatti1220@gmail.com>
2022-07-12 19:56:44 -0300

    style: change code layout based on solidity docs standard

--- smartcontract/contracts/CryptoDevToken.sol
+++ smartcontract/contracts/CryptoDevToken.sol
@@ -1,64 +1,64 @@
 // SPDX-License-Identifier: MIT
-  pragma solidity ^0.8.4;
+pragma solidity ^0.8.4;
 
-  import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-  import "@openzeppelin/contracts/access/Ownable.sol";
-  import "./interfaces/ICryptoDevs.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/ICryptoDevs.sol";
 
-  contract CryptoDevToken is ERC20, Ownable {
-      uint256 public constant tokenPrice = 0.001 ether;
-      uint256 public constant tokensPerNFT = 10 * 10**18;
-      uint256 public constant maxTotalSupply = 10000 * 10**18;
-      
-      ICryptoDevs CryptoDevsNFT;
+contract CryptoDevToken is ERC20, Ownable {
+    uint256 public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+    uint256 public constant maxTotalSupply = 10000 * 10**18;
 
-      mapping(uint256 => bool) public tokenIdsClaimed;
+    ICryptoDevs CryptoDevsNFT;
 
-      constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
-          CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
-      }
+    mapping(uint256 => bool) public tokenIdsClaimed;
 
-      function mint(uint256 amount) public payable {
-          uint256 _requiredAmount = tokenPrice * amount;
-          require(msg.value >= _requiredAmount, "Insufficient funds");
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
+    }
 
-          uint256 amountWithDecimals = amount * 10**18;
+    receive() external payable {}
 
-          require(
-              (totalSupply() + amountWithDecimals) <= maxTotalSupply,
-              "Total supply exceeded"
-          );
+    fallback() external payable {}
 
-          _mint(msg.sender, amountWithDecimals);
-      }
+    function mint(uint256 amount) public payable {
+        uint256 _requiredAmount = tokenPrice * amount;
+        require(msg.value >= _requiredAmount, "Insufficient funds");
 
-      function claim() public {
-          address sender = msg.sender;
-          uint256 balance = CryptoDevsNFT.balanceOf(sender);
-          require(balance > 0, "You dont own Crypto Dev NFT's");
+        uint256 amountWithDecimals = amount * 10**18;
 
-          uint256 amount = 0;
+        require(
+            (totalSupply() + amountWithDecimals) <= maxTotalSupply,
+            "Total supply exceeded"
+        );
 
-          for (uint256 i = 0; i < balance; i++) {
-              uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
-              if (!tokenIdsClaimed[tokenId]) {
-                  amount += 1;
-                  tokenIdsClaimed[tokenId] = true;
-              }
-          }
+        _mint(msg.sender, amountWithDecimals);
+    }
 
-          require(amount > 0, "You have claimed all the tokens");
-          _mint(msg.sender, amount * tokensPerNFT);
-      }
+    function claim() public {
+        address sender = msg.sender;
+        uint256 balance = CryptoDevsNFT.balanceOf(sender);
+        require(balance > 0, "You dont own Crypto Dev NFT's");
 
-      function withdraw() public onlyOwner {
+        uint256 amount = 0;
+
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+            if (!tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+
+        require(amount > 0, "You have claimed all the tokens");
+        _mint(msg.sender, amount * tokensPerNFT);
+    }
+
+    function withdraw() public onlyOwner {
         address _owner = owner();
         uint256 amount = address(this).balance;
         (bool sent, ) = _owner.call{value: amount}("");
         require(sent, "Failed to send Ether");
-      }
-
-      receive() external payable {}
-
-      fallback() external payable {}
-  }
+    }
+}
