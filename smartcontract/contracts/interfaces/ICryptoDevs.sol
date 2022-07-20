50db4bd30da0c52194fa72e464182c567954c3d4
AlanRacciatti <alanracciatti1220@gmail.com>
2022-07-19 21:45:19 -0300

    feat: add DAO and Fake NFT Marketplace contracts

--- smartcontract/contracts/interfaces/ICryptoDevs.sol
+++ smartcontract/contracts/interfaces/ICryptoDevs.sol
@@ -2,14 +2,10 @@
 pragma solidity ^0.8.4;
 
 interface ICryptoDevs {
-    /**
-     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
-     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
-     */
     function tokenOfOwnerByIndex(address owner, uint256 index)
         external
         view
-        returns (uint256 tokenId);
+        returns (uint256);
 
-    function balanceOf(address owner) external view returns (uint256 balance);
+    function balanceOf(address owner) external view returns (uint256);
 }
